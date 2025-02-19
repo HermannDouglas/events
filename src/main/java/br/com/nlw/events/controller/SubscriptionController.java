c87b91778ee1a0c9c31bd853842948cf3e402fae
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-02-19 12:00:49 -0500

    Feat: Adicionar suporte a usuário indicador na criação de assinaturas
    - Modifica o método createSubscription na SubscriptionController para aceitar um parâmetro opcional userId.
    - Atualiza o método createNewSubscription na SubscriptionService para incluir a lógica de verificação do usuário indicador.
    - Adiciona tratamento de exceção para UserIndicadorNotFoundException na SubscriptionController.

--- src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -11,6 +11,7 @@ import br.com.nlw.events.dto.ErrorMessage;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.SubscriptionConflictException;
+import br.com.nlw.events.exception.UserIndicadorNotFoundException;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
 import br.com.nlw.events.service.SubscriptionService;
@@ -21,10 +22,11 @@ public class SubscriptionController {
     @Autowired
     private SubscriptionService service;
 
-    @PostMapping("/subscription/{prettyName}")
-    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber) {
+    @PostMapping({ "/subscription/{prettyName}", "/subscription/{prettyName}/{userId}" })
+    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber,
+            @PathVariable(required = false) Integer userId) {
         try {
-            SubscriptionResponse res = service.createNewSubscription(prettyName, subscriber);
+            SubscriptionResponse res = service.createNewSubscription(prettyName, subscriber, userId);
             if (res != null) {
                 return ResponseEntity.ok(res);
             }
@@ -32,6 +34,8 @@ public class SubscriptionController {
             return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
         } catch (SubscriptionConflictException ex) {
             return ResponseEntity.status(409).body(new ErrorMessage(ex.getMessage()));
+        } catch(UserIndicadorNotFoundException ex) {
+            return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
         }
         return ResponseEntity.badRequest().build();
     }
--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.SubscriptionConflictException;
+import br.com.nlw.events.exception.UserIndicadorNotFoundException;
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
@@ -25,7 +26,7 @@ public class SubscriptionService {
     @Autowired
     private SubscriptionRepo subRepo;
 
-    public SubscriptionResponse createNewSubscription(String eventName, User user) {
+    public SubscriptionResponse createNewSubscription(String eventName, User user, Integer userId) {
 
         Event evt = evtRepo.findByPrettyName(eventName);
         if (evt == null) {
@@ -37,9 +38,15 @@ public class SubscriptionService {
             userRec = userRepo.save(user);
         }
 
+        User indicador = userRepo.findById(userId).orElse(null);
+            if (indicador == null) {
+                throw new UserIndicadorNotFoundException("Usuário "+ userId +" indicador não existe!");
+            }
+
         Subscription subs = new Subscription();
         subs.setEvent(evt);
         subs.setSubscriber(userRec);
+        subs.setIndication(indicador);
 
         Subscription tmpSub = subRepo.findByEventAndSubscriber(evt, userRec);
         if (tmpSub != null) {
